020916ca7290f0a0fdd6b7aff12c45112428f6d9
donno2048 <just4now666666@gmail.com>
2022-08-23 20:52:08 +0300

    assume the user already installed the package

--- pybracket/__main__.py
+++ pybracket/__main__.py
@@ -1,10 +1,5 @@
 from sys import argv
-try:
-    from . import main as register
-except ImportError:
-    from pybracket import main as register
 def main():
-    register()
     if len(argv) == 1:
         raise Exception("No file passed to execute")
     exec(open(argv[1], "rb").read().decode("brackets"))
